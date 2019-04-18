fc5071e6f49ba83031b75e1f0ab923a26ecec903
matthew-trump <matthew.trump@gmail.com>
2019-04-18 15:36:21 -0700

    adjust routes

--- server.js
+++ server.js
@@ -3,29 +3,24 @@ const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser');
 const api = require('./api');
-const { login, handleUnauthorizedError, checkIfAuthenticated } = require('./login');
+const { jwtAuthorization, jwtLogin, jwtUnauthorizedError } = require('./auth/jwt-auth');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-//app.use(express.urlencoded());
 
 app.use('/', express.static(path.join(__dirname, 'public')))
-
+app.use("/login", jwtLogin);
 app.use("/api",
-    checkIfAuthenticated,
-    handleUnauthorizedError,
+    jwtAuthorization,
+    jwtUnauthorizedError,
     api);
-
-app.use("/login", login);
-
-//app.use('/api/ttsaudio', express.static(path.join(__dirname, 'ttsaudio')))
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Dialogflow Testing Tool App listening on port ${PORT}`);
+    console.log(`Dialogflow Webhook Tester App listening on port ${PORT}`);
     console.log(`ENVIRONMENT ${process.env.ENVIRONMENT}`)
     console.log(`DEFAULT_TARGET ${process.env.DEFAULT_TARGET}`)
     console.log('Press Ctrl+C to quit.');
