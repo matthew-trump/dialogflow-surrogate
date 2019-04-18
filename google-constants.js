c5e977b7d7ecbd08d8f0e87d91bdb293cc8a8dd1
matthew-trump <matthew.trump@gmail.com>
2019-04-18 15:35:22 -0700

    constants used in app related to gcp

---
+++ google-constants.js
@@ -0,0 +1,12 @@
+const APP_DATA_CONTEXT = '_actions_on_google';
+
+const ACTIONS_CAPABILITY_CUSTOM_STAGE = "actions_capability_custom_stage";
+const ACTIONS_CAPABILITY_SCREEN_OUTPUT = "actions_capability_screen_output";
+const ACTIONS_CAPABILITY_AUDIO_OUTPUT = "actions_capability_audio_output";
+const ACTIONS_CAPABILITY_WEB_BROWSER = "actions_capability_web_browser";
+const ACTIONS_CAPABILITY_MEDIA_RESPONSE_AUDIO = "actions_capability_media_response_audio";
+
+const GOOGLE_ASSISTANT_INPUT_TYPE_KEYBOARD = "google_assistant_input_type_keyboard";
+
+const INPUT_TYPE_KEYBOARD = "KEYBOARD";
+const INPUT_TYPE_VOICE = "VOICE";
